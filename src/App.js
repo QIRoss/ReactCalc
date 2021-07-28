d982f7968fc01428cf6b97a9410750c10dcd60ad
Lucas de Queiroz <lucasqreis@poli.ufrj.br>
2021-07-28 10:05:18 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Digit from "./components/Digit";
+import Display from "./components/Display";
+import { digits } from "./data/digits";
 
 function App() {
+  const [currentValue, setCurrentValue] = useState("");
+  const [operatorValue, setOperatorValue] = useState("");
+  const [result, setResult] = useState("");
+  const [operand, setOperand] = useState("");
+
+  const updateValue = (val) => {
+    if (val === "." && currentValue.includes(".")) {
+      setCurrentValue(currentValue);
+    } else {
+      let appendDigits = currentValue + val;
+      if (appendDigits.charAt(0) === "0") {
+        appendDigits = appendDigits.slice(1);
+      }
+      setCurrentValue(appendDigits);
+    }
+  };
+
+  const performOperation = (val) => {
+    if (val === "=") {
+      calculate();
+    } else if (val === "AC") {
+      clearDisplayField();
+    } else if (val === "-" && currentValue.length === 0) {
+      setCurrentValue(val + currentValue);
+    } else if (currentValue === "-" && operand) {
+      setOperatorValue(val);
+      setCurrentValue("");
+    } else {
+      setOperatorValue(val);
+      calculate();
+    }
+  };
+
+  const calculate = () => {
+    if (currentValue && operand) {
+      let operationResult;
+      let value1 = parseFloat(currentValue);
+      let value2 = parseFloat(operand);
+
+      switch (operatorValue) {
+        case "+":
+          operationResult = value2 + value1;
+          break;
+        case "-":
+          operationResult = value2 - value1;
+          break;
+        case "*":
+          operationResult = value2 * value1;
+          break;
+        case "/":
+          operationResult = value2 / value1;
+          break;
+        default:
+          return;
+      }
+      setCurrentValue("");
+      setResult(operationResult);
+      setOperand(operationResult);
+    } else if (currentValue === "-") {
+      setOperand("");
+    } else if (currentValue) {
+      setOperand(currentValue);
+      setCurrentValue("");
+    }
+  };
+
+  const clearDisplayField = () => {
+    setResult("");
+    setCurrentValue("");
+    setOperand("");
+    setOperatorValue("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Display
+        currentValue={currentValue}
+        operand={operand}
+        result={result}
+        operatorValue={operatorValue}
+      />
+      <div className="calculator">
+        {digits.map((digit) => {
+          return (
+            <Digit
+              key={digit.id}
+              id={digit.id}
+              value={digit.value}
+              updateValue={updateValue}
+              performOperation={performOperation}
+              operation={digit.operation}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/Display.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./Display.css";
+
+export default function Display({
+  currentValue,
+  operand,
+  result,
+  operatorValue
+}) {
+  const [blink, setBlink] = useState(false);
+  const changeDisplayField = () => {
+    if (result.toString()) {
+      return result;
+    } else if (currentValue.toString()) {
+      return currentValue;
+    } else if (operand.toString()) {
+      return operand;
+    } else {
+      return "0";
+    }
+  };
+
+  useEffect(() => {
+    setBlink(true);
+    const timer = setTimeout(() => {
+      setBlink(false);
+    }, 200);
+    return () => clearTimeout(timer);
+  }, [operatorValue, result]);
+
+  return (
+    <div className="Display" id="display">
+      <span className={blink ? "blink" : ""}>{changeDisplayField()}</span>
+    </div>
+  );
+}
